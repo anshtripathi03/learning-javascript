8b971fe32af358c9bc32a4d8d2a7812855d132c7
Ansh Tripathi <ansh.tripathi17012006@gmail.com>
2025-08-08 10:40:28 +0000

    video 3 done

--- 01@basics/test.js
+++ 01@basics/test.js
@@ -1,1 +1,6 @@
-console.log("ansh")
+"use strict" //use newest version of javascirpt
+
+// alert(3+3) // we are using node not browser
+
+console.log(typeof null) // null is an object
+console.log(typeof undefined) // undefined is undefined
